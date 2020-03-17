6ec4c29c906845571339f42153e94fab03174ac6
Hedzr Yeh <hedzrz@gmail.com>
2020-03-17 18:02:33 +0800

    main entry codes

--- mdx/Program.cs
+++ mdx/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Diagnostics;
+using mdxlib.MDict;
 
 namespace mdx
 {
+    /// <summary>
+    ///
+    /// ll
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            foreach (var filename in args)
+            {
+                using var l = new MDictLoader(filename);
+                try
+                {
+                    l.Process();
+                    Console.WriteLine($"header: {l.DictHeader}");
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.ToString());
+                    // throw;
+                }
+                finally
+                {
+                    // l.Dispose();
+                }
+            }
+
+            // mdxlib.open("*.mdx,mdd,sdx,wav,png,...") => mdxfile
+            // mdxfile.preload()
+            // mdxfile.getEntry("beta") => entryInfo.{item,index}
+            // mdxfile.find("a")           // "a", "a*b", "*b"
+            // mdxfile.close()
+            // mdxfile.find()
+            // mdxfile.find()
+            // mdxfile.find()
         }
     }
 }
