af66b9f2bedf614935604b3cef9f81a182c7f18e
Hedzr Yeh <hedzrz@gmail.com>
2020-03-28 21:02:44 +0800

    suggestion, auto-suggestion based on Jaro-Winker distance calculating.

--- src/HzNS.Cmdr.Core/Internal/DefaultMatcher.cs
+++ src/HzNS.Cmdr.Core/Internal/DefaultMatcher.cs
@@ -115,7 +115,7 @@ namespace HzNS.Cmdr.Internal
                     else
                     {
                         // treat as remains args normally
-                        
+
                         // @this.logDebug("level {Level} (no sub-cmds): returning {Position}", level, matchedPosition);
                         // onCommandCannotMatched(@this, args, i, arg, command);
                         return matchedPosition;
@@ -211,14 +211,14 @@ namespace HzNS.Cmdr.Internal
                     }
                     else
                     {
-                    pos += decidedLen;
+                        pos += decidedLen;
                         len = 1;
                     }
 
                     @this.logDebug("    - for next part: {Part}", fragment.Substring(pos, len));
                     ccc = command;
                     if (len > 0)
-                    goto forEachFragmentParts;
+                        goto forEachFragmentParts;
                 }
 
                 #endregion
@@ -620,28 +620,66 @@ namespace HzNS.Cmdr.Internal
             in bool longOpt, ICommand cmd)
             where T : IDefaultMatchers
         {
-            var xref = @this.xrefs[cmd];
-            if (longOpt)
+            do
             {
-                @this.suggestFor(fragment, xref.FlagsLongNames);
-                @this.suggestFor(fragment, xref.FlagsAliasNames);
-            }
-            else
+                var xref = @this.xrefs[cmd];
+                if (longOpt)
+                {
+                    @this.suggestFor(fragment, longOpt, xref.FlagsLongNames);
+                    @this.suggestFor(fragment, longOpt, xref.FlagsAliasNames);
+                }
+                else
+                {
+                    @this.suggestFor(fragment, longOpt, xref.FlagsShortNames);
+                }
+
+                if (!cmd.IsRoot)
+                    cmd = cmd.Owner!;
+            } while (!cmd.IsRoot);
+        }
+
+        /// <summary>
+        /// <code>bool OnSuggestingForCommand(object worker,
+        ///     Dictionary&lt;string, ICommand&gt; dataset, string token)</code>
+        /// </summary>
+        public static Func<object, Dictionary<string, ICommand>, string, bool>? OnSuggestingForCommand { get; set; }
+
+        /// <summary>
+        /// <code>bool OnSuggestingForFlag(object worker,
+        ///     Dictionary&lt;string, IFlag&gt; dataset, string token)</code>
+        /// </summary>
+        public static Func<object, Dictionary<string, IFlag>, string, bool>? OnSuggestingForFlag { get; set; }
+
+        [SuppressMessage("ReSharper", "UnusedParameter.Local")]
+        private static void suggestFor<T>(this T @this, string token, Dictionary<string, ICommand> dataset)
+            where T : IDefaultMatchers
+        {
+            if (OnSuggestingForCommand?.Invoke(@this, dataset, token) == true) return;
+
+            foreach (var (key, opt) in dataset)
             {
-                @this.suggestFor(fragment, xref.FlagsShortNames);
+                var rate = JaroWinkler.RateSimilarity(token, key);
+                if (rate > 0.73)
+                    errPrint($"  - maybe \"{opt.Long}\""); // under \"{opt.Owner?.backtraceTitles}\"");
             }
         }
 
         [SuppressMessage("ReSharper", "UnusedParameter.Local")]
-        private static void suggestFor<T>(this T @this, string tag, Dictionary<string, ICommand> dataset)
+        private static void suggestFor<T>(this T @this, string token, bool longOpt, Dictionary<string, IFlag> dataset)
             where T : IDefaultMatchers
         {
-        }
+            if (OnSuggestingForFlag?.Invoke(@this, dataset, token) == true) return;
 
-        [SuppressMessage("ReSharper", "UnusedParameter.Local")]
-        private static void suggestFor<T>(this T @this, string tag, Dictionary<string, IFlag> dataset)
-            where T : IDefaultMatchers
-        {
+            foreach (var (key, opt) in dataset)
+            {
+                var rate = JaroWinkler.RateSimilarity(token, key);
+                // ReSharper disable once InvertIf
+                if (rate > 0.73)
+                {
+                    var sw = Util.SwitchChar(longOpt);
+                    errPrint($"  - Maybe \"{sw}{opt.Long}\"?"); // (under \"{opt.Owner?.backtraceTitles}\")");
+                }
+            }
         }
 
         #endregion
@@ -652,7 +690,14 @@ namespace HzNS.Cmdr.Internal
         // ReSharper disable once InconsistentNaming
         private static void errPrint(string message)
         {
-            Console.Error.WriteLine(message);
+            _errorWriter.WriteLineAsync(message);
+        }
+
+        static StringWriter _errorWriter = new StringWriter();
+
+        public static void FlushErrors<T>(this T @this) where T : IDefaultMatchers
+        {
+            Console.Error.WriteLineAsync(_errorWriter.ToString());
         }
 
         #endregion
@@ -858,5 +903,119 @@ namespace HzNS.Cmdr.Internal
         };
 
         #endregion
+    }
+
+    class JaroWinkler
+    {
+        private const double DefaultMismatchScore = 0.0;
+        private const double DefaultMatchScore = 1.0;
+
+        /// <summary>
+        /// Gets the similarity between two strings by using the Jaro-Winkler algorithm.
+        /// A value of 1 means perfect match. A value of zero represents an absolute no match
+        /// </summary>
+        /// <param name="firstWord"></param>
+        /// <param name="secondWord"></param>
+        /// <returns>a value between 0-1 of the similarity</returns>
+        /// 
+        public static double RateSimilarity(string? firstWord, string? secondWord)
+        {
+            if (firstWord == null || secondWord == null) return DefaultMismatchScore;
+
+            // Converting to lower case is not part of the original Jaro-Winkler implementation
+            // But we don't really care about case sensitivity in DIAMOND and wouldn't decrease security names similarity rate just because
+            // of Case sensitivity
+            firstWord = firstWord.ToLower();
+            secondWord = secondWord.ToLower();
+
+            if (firstWord == secondWord)
+                //return (SqlDouble)defaultMatchScore;
+                return DefaultMatchScore;
+
+
+            {
+                // Get half the length of the string rounded up - (this is the distance used for acceptable transpositions)
+                var halfLength = Math.Min(firstWord.Length, secondWord.Length) / 2 + 1;
+
+                // Get common characters
+                var common1 = GetCommonCharacters(firstWord, secondWord, halfLength);
+                var commonMatches = common1?.Length ?? 0;
+
+                // Check for zero in common
+                if (commonMatches == 0)
+                    //return (SqlDouble)defaultMismatchScore;
+                    return DefaultMismatchScore;
+
+                var common2 = GetCommonCharacters(secondWord, firstWord, halfLength);
+
+                // Check for same length common strings returning 0 if is not the same
+                if (commonMatches != common2?.Length)
+                    //return (SqlDouble)defaultMismatchScore;
+                    return DefaultMismatchScore;
+
+                // Get the number of transpositions
+                int transpositions = 0;
+                for (int i = 0; i < commonMatches; i++)
+                {
+                    if (common1 != null && common1[i] != common2[i])
+                        transpositions++;
+                }
+
+                int j = 0;
+                j += 1;
+
+                // Calculate Jaro metric
+                transpositions /= 2;
+                double jaroMetric = commonMatches / (3.0 * firstWord.Length) +
+                                    commonMatches / (3.0 * secondWord.Length) +
+                                    (commonMatches - transpositions) / (3.0 * commonMatches);
+                //return (SqlDouble)jaroMetric;
+                return jaroMetric;
+            }
+
+            //return (SqlDouble)defaultMismatchScore;
+        }
+
+        /// <summary>
+        /// Returns a string buffer of characters from string1 within string2 if they are of a given
+        /// distance seperation from the position in string1.
+        /// </summary>
+        /// <param name="firstWord">string one</param>
+        /// <param name="secondWord">string two</param>
+        /// <param name="separationDistance">separation distance</param>
+        /// <returns>A string buffer of characters from string1 within string2 if they are of a given
+        /// distance seperation from the position in string1</returns>
+        private static StringBuilder? GetCommonCharacters(string firstWord, string secondWord, int separationDistance)
+        {
+            if ((firstWord != null) && (secondWord != null))
+            {
+                var returnCommons = new StringBuilder(20);
+                var copy = new StringBuilder(secondWord);
+                var firstWordLength = firstWord.Length;
+                var secondWordLength = secondWord.Length;
+
+                for (int i = 0; i < firstWordLength; i++)
+                {
+                    char character = firstWord[i];
+                    bool found = false;
+
+                    for (int j = Math.Max(0, i - separationDistance);
+                        !found && j < Math.Min(i + separationDistance, secondWordLength);
+                        j++)
+                    {
+                        if (copy[j] == character)
+                        {
+                            found = true;
+                            returnCommons.Append(character);
+                            copy[j] = '#';
+                        }
+                    }
+                }
+
+                return returnCommons;
+            }
+
+            return null;
+        }
     }
 }
